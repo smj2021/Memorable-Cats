1a11f82282b27349a84064829822c69f34f96335
smj2021 <mjschwartz515@gmail.com>
2021-09-26 21:26:30 -0400

    made a grid using DOM events

--- memorablecats.js
+++ memorablecats.js
@@ -1,82 +1,93 @@
+// Pseudocode
+
+// What should the game do?
+
+// 1)When the game is initialized, the winConditions variable array should be declared with the required paired arrays already inside.
+    // 1.1) The empty array container arrayOfCats for the tiles should be declared first, followed by the winConditions.
+    // 1.2) Init should also execute the randomization function to randomly assign images to different tiles.
+    // 1.3) Init needs to set / reset the timer as well.
+
+// 2) When a card/tile is clicked, the game clock should begin counting up.A handleClick function should contain code to either execute clock start, card / tile flip, and matching or call additional functions that do so.
+    // 2.1) When the second tile is clicked, the game should determine if it is a match to the first clicked tile or not.
+    // 2.2) If it is, both tiles should become unusable and remain flipped.If the tiles don’t match, they should return to their original state.
+    // 2.3) On click, tiles should flip over(or disappear) revealing the picture underneath, and remain in this state until the player clicks a second tile.
+
+// The render function should
+
+// The game is over when the player has matched all the available tiles.This state should stop the clock.
+// Once the clock stops, the message line should read “Congratulations, you matched all the cards in XXXX minutes / seconds!” or something similar.
+
+// The reset button should be available after the first card / tile is clicked.
+
+
+
 //-------CONSTANTS-------//
-let cardArray = [];
-let winConditions = [[], [], [], [], [], [], [], []];//Contains the pairs of matching cards to compare the playerChoices against in the isWinner function.
+let cardArray = [{name: 'cat1', img: 'images/IMG_1.jpeg'},
+    {name: 'cat2', img: 'images/IMG_2.jpeg'},
+    {name: 'cat3', img: 'images/IMG_3.jpeg'},
+    {name: 'cat4', img: 'images/IMG_4.jpeg'},
+    {name: 'cat5', img: 'images/IMG_5.jpeg'},
+    {name: 'cat6', img: 'images/IMG_6.jpeg'},
+    {name: 'cat7', img: 'images/IMG_7.jpeg'},
+    {name: 'cat8', img: 'images/IMG_8.jpeg'},
+];
+// let winConditions = [[], [], [], [], [], [], [], []];//Contains the pairs of matching cards to compare the playerChoices against in the isWinner function.
 
 
 //-------VARIABLES(STATE)-------//
-
+// let isWinner;//This will tell us if there's a winner
+let playerChoices = [];//This is the array for card clicks to be logged.
+let playerChoicesIden = [];//
 
 
 //-------CACHED ELEMENT REFERENCES-------//
+const cardGrid = document.querySelector('card-grid');//grabs the div container that will hold the cards
+
 const messageEl = document.getElementById("message");
 const cardsEl = document.querySelectorAll('section > div');
 const cardArrayEl = document.querySelector('.cardArray');
-const resetBtn = document.querySelector("reset");
+
 
 
 //-------EVENT LISTENERS-------//
-cardArrayEl.addEventListener('click', handleClick);
-resetBtn.addEventListener('reset', init);
+cardEl.addEventListener('click', flipCard);
+
+
+
+
 
 //-------FUNCTI0NS-------//
-init();
+
+function makeGrid(){
+    for (let i =0; i < cardArray.length; i++){
+        let cardEl = document.createElement('image');
+        cardel.setAttribute('src', 'images/blank.jpeg');//should link cards to image sources.
+        cardEl.setAttribute('id', i);//assigns index positions to each card in the array
+        cardEl.addEventListener('click', flipCard);//add an event listener to watch for click
+
+    }
+}
 
 function init() {
-    matchedCards = [null];
-    winConditions = [1, 2, 3, 4, 5, 6];
-    cardArray = [];
+    
+    winConditions = [];
+    matchedCards = [];//Card matches will push into this array and when it fills
 
-    // resetBtn.setAttribute('hidden', true);
     messageEl.innerText = "Click a card to pick a card and start the timer!";
-    // render();
+   
 }
 
-function flipCard() {
-
-}
-
-
-// Need a shuffle function to randomize card placement. Could make simple Math.floor(Math.random)
-
-
-
-function handleClick(event) {//When cards/tiles are clicked in the array, the indexes of the board are mapped to the squares.
-    const clickEl = event.target;
-    const clickedElIdx = clickE.id;
-    if (cardArray[clickedElIdx] !== null) {
-        return;
-    }
-    if (winConditions[5] == null) {
-        return;
-    }
-    cardArray[clickedElIdx] <= 15;
-    render();
-
-
-
-    function render() {
-        for (let i = 0; i < cardArray.length; i++) {//As we loop through the board array we are accessing the squares array at the same time. 
-            if (cardArray[i] === null) {
-                cardsEl[i].style.background = 'tan';//3.3.1.2
-            } else if (cardArray[i] <= 15) {
-                cardsEl[i].style.background = 'orange';
-                cardsEl[i].innerText = 'X';
-                resetBtn.removeAttribute("hidden");
-            } else if (cardArray[i] === -1) {
-                cardsEl[i].style.background = 'purple';//3.3.1.2)
-                cardsEl[i].innerText = 'O';
-                console.log(cardArray[i]);
-            }//3.3.2
-            console.log(cardArray);
-        }
+function flipCard(){//flip cards on click
+    let cardIden = this.getAttribute('id');
+    playerChoices.push(cardArray[cardIden].name);
+    playerChoicesIden.push(CardIden);
+    this.setAttribute('src', cardArray[cardIden].img);
+    if (playerChoices.length === 2){
+        setTimeout(matchCards, 500);
     }
 }
 
+function matchCards(){}
 
-function getWinner() {
-    if (winConditions[5] !== null && winConditions[5] !== '') {
-        messageEl.innerText = 'Congratulations Player, you\'ve won the game!';
-    } else {
-        return;
-    }
-}
+
+    function render() {}

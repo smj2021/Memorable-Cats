37377d6b9ce13baa7e418b92fc7bd6dd0060abfa
smj2021 <mjschwartz515@gmail.com>
2021-09-30 15:15:39 -0400

    began cleaning up dead code and unused variables

--- memorablecats.js
+++ memorablecats.js
@@ -1,4 +1,5 @@
 //-------CONSTANTS-------//
+//cardArray allows the board to be drawn dynamically as well as allows for the implementation of a shuffle function.
 let cardArray = [
     { name: '0', img: 'images/IMG_1.JPG' },
     { name: '0', img: 'images/IMG_1.JPG' },
@@ -17,7 +18,6 @@ let cardArray = [
     { name: '7', img: 'images/IMG_8.JPG' },
     { name: '7', img: 'images/IMG_8.JPG' }
 ];
-// let winConditions = [[], [], [], [], [], [], [], []];//Contains the pairs of matching cards to compare the playerChoices against in the isWinner function.
 
 
 //-------VARIABLES(STATE)-------//
@@ -34,18 +34,23 @@ let matchCombos = [
     ['IMG_7.JPG', 'IMG_7.JPG'],
     ['IMG_8.JPG', 'IMG_8.JPG']
 ];
-let firstCard;//First card chosen by a player
-let secondCard;//Second card chosen by a player
-// const elementZero = document.querySelector('#0');
-// console.log(elementZero);
+
+
+//First card chosen by a player
+let firstCard;
+//Second card chosen by a player
+let secondCard;
 
 //-------CACHED ELEMENT REFERENCES-------//
-const cardGridEl = document.querySelectorAll('.grid');//grabs the section container that will hold the cards
+//Grabs the section container that will hold the cards
 const cardGridSectionEl = document.querySelector('#cardgrid');
-//grabs the container holding the cards
+// const cardGridEl = document.querySelectorAll('.grid');
+//Displays all messages relating to game state.
 const messageEl = document.querySelector('#message');
-const cardsNodeList = document.querySelectorAll('.cards');//grabs all divs for each card
-const resetBtn = document.querySelector('#reset-button');//grabs the reset button
+// const cardsList = document.querySelectorAll('.cards');
+//Grabs the reset button
+const resetBtn = document.querySelector('#reset-button');
+//Yet unimplemented tiner for tracking solution speed.
 const timerEl = document.querySelector('#timer');
 
 
@@ -63,17 +68,21 @@ createBoard();
 function init() {
     playerChoices = [];
     playerChoicesIdx = [];
-    // let matches = document.querySelectorAll('img');
     playerMatches = [];//matched pairs of cards will be stored here after first and second click are made.
     resetBtn.setAttribute('hidden', init);
     messageEl.innerText = 'Player, pick a card to start the clock!';
-    // render();
-    // cardsNodeList.setAttribute.classList.remove('active');
-
-
 }
 
+// Generates the board itself, and executes the function appending cards to the board.
+function createBoard() {
+    let counter = 0;
+    for (let i = 0; i < cardArray.length; i++) {
+        if (i % 2 === 0) counter++;
+        createAndAppendCardEls(i, counter);
+    }
+}
 
+// Generates the cards on the board.
 function createAndAppendCardEls(idValue, imgNum) {
     let div = document.createElement('div');
     div.setAttribute('id', idValue);
@@ -90,56 +99,34 @@ function createAndAppendCardEls(idValue, imgNum) {
     div.appendChild(frontImage);
     div.appendChild(backImage);
     cardGridSectionEl.appendChild(div);
-    // console.log(cardGridSectionEl);
-}
-
-
-function createBoard() {
-    let counter = 0;//counter starts at 1 and every even loop increments.
-    for (let i = 0; i < cardArray.length; i++) {
-        //console.log(counter);
-        if (i % 2 === 0) counter++;
-        createAndAppendCardEls(i, counter);
-    }
 }
 
 //This function responds to the player clicking a card and executes cardMatchCheck() if the playerChoices array holds two values.
 function handleClick(e) {
     e.target.parentNode.classList.add('active');
-    console.log(e.target.parentNode.classList);
-    //let clickedEl = e.target.parentNode.classList['name'];
-    //console.log(clickedEl); //why is this undefined?
     playerChoices.push(e.target.parentNode.firstChild['name']);
-    console.log(playerChoices);
     if (playerChoices.length === 2) {
         cardMatchCheck();
+    } else {
+        // How can I turn the cards back over   
     }
-
 }
 
 
 
 //This function checks to see if index 1 and index 2 in the playerChoices array are the same card based on a card name string stored in playerChoices on click.
 function cardMatchCheck() {
-
     firstCard = playerChoices[0];
     secondCard = playerChoices[1];
     if (playerChoices[0] === playerChoices[1]) {
         messageEl.innerText = 'Congratulations, Player! You found a match!';
-
         playerMatches.push(playerChoices);
-        console.log(playerMatches);
         playerChoices.length = 0;
-        console.log(playerChoices);
-
     }
     else {
         messageEl.innerText = 'That wasn\'t a match. Pick again!'
         playerChoices.length = 0;
-        playerChoices.classList.removeAttribute('active');
     }
-
-    // cardGridSectionEl.removeEventListener('click', handleClick);
     checkWin();
 }
 
@@ -153,7 +140,8 @@ function checkWin() {
     return;
 }
 
-function reload(){
+//Reloads the game when the 'Play Again' button is clicked.
+function reload() {
     location.reload();
 }
 
@@ -185,5 +173,3 @@ function reload(){
 //         cardsEl[secondCard].setAttribute('src', 'images/blank.JPEG');
 //     }
 // }
-
-// function render() {}
